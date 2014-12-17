793453a47d93711402e4771f3b72943ed8e05c6a
m-marini <marco.marini@mmarini.org>
2014-12-17 08:53:54 +0100

    parameterized main

--- briscala/octave/runnit.sh
+++ briscala/octave/runnit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in $(eval echo {1..$1})
 do
-    gnome-terminal -x octave --persist --eval 'main("file'$i'.mat")'
+    gnome-terminal -x octave --persist --eval 'main("best-'$i'.mat")'
 done
 
